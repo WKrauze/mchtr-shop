28cb5831f4aafe96a1346c8f15af561f4205f82b
Wojciech Krauze <w.krauze@hotmail.com>
2020-01-08 11:12:37 +0100

    Updated report for expensive purchases, updated euro rate, updated tender_cpv list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-# mchtr-shop 0.3
+# mchtr-shop 0.4
+# LICENSE: MIT
 
 from fpdf import FPDF
 from sys import platform
@@ -8,6 +9,11 @@ if platform == "linux" or platform == "linux2":
     system = 'linux'
 elif platform == "win32":
     system = 'windows'
+
+euro = 4.2693
+forbidden_cpv_codes = ('302', '380', '385', '386')
+tender_threshold_research_euro = 214000*euro
+tender_threshold_nonresearch_euro = 30000*euro
 
 pdf = FPDF()
 pdf.add_page()
@@ -26,8 +32,6 @@ while i:
         i = False
     except ValueError:
         print("Błędny kod CPV. Spróbuj ponownie")
-
-forbidden_cpv_codes = ('302', '323', '386', '426', '429', '484')
 
 if str(cpv)[0:3] in forbidden_cpv_codes:
     tender_cpv = True
@@ -87,31 +91,35 @@ else:
 pdf.multi_cell(180, 7, txt=str(num) + ". Złóż wniosek do działu administracyjnego (p. 627). ")
 num += 1
 
-# TODO: sprawdzić jaki jest warunek kwotowy dla przetargu
 if (not tender_cpv and price < 1000) or (tender_cpv and research == 'tak' and price < 1000):
     rule = 'nie podlega ustawie Prawo zamówień publicznych.'
     rule_num = 0
-elif tender_cpv and research == 'tak':
+elif research == 'tak' and price < tender_threshold_research_euro:
     rule = 'podlega Zarządzeniu Dziekana nr 03/2016.'
     rule_num = 1
-elif (not tender_cpv) and (price < 100000):
+elif (not tender_cpv) and (price < tender_threshold_nonresearch_euro):
     rule = 'podlega Zarządzeniu Dziekana nr 8/2014.'
     rule_num = 1
-elif tender_cpv:
+else:
     rule = 'podlega pod przetarg nieograniczony.'
     rule_num = 2
 pdf.multi_cell(180, 7, txt=str(num) + ". Po 1-2 dniach wniosek jest uzupełniony. Sprawdź wg jakich zapisów "
-                                       "powinnaś/powinieneś realizować zakup. Zgodnie z Twoimi odpowiedziami, "
+                                       "należy realizować zakup. Zgodnie z Twoimi odpowiedziami, "
                                        "twój zakup  " + rule)
 num += 1
 
 if rule_num == 2:
+    pdf.multi_cell(180, 7, txt=str(num) + ". Przygotuj opis przedmiotu zamówienia w którym opiszesz pożądaną specyfikację zamawianego towaru."
+                                          " Przykładowy opis znajdziesz w pliku opis-przedmiotu-zamowienia.odt.")
+    num += 1
+    pdf.multi_cell(180, 7, txt=str(num) + ". Opis przedmiotu zamówienia razem z podpisanym wnioskiem zanieś do p. Marii Lepy (1. piętro), która"
+                                          " przejmie realizację przetargu.")
     pdf.output("plan-realizacji-zakupu.pdf")
-    sys.exit('Zakup przetargowy nie jest jeszcze opracowany. Generowanie cząstkowego raportu.')
+    sys.exit()
 
 
-# Sekcja 2 - przegląd 3 ofert/zaproszenie do składania ofert/zakup z wolnej ręki
-if rule_num != 0:
+# Sekcja 2 - zebranie ofert
+if rule_num == 1:
     pdf.multi_cell(180, 7, txt="ZEBRANIE OFERT", align="C")
     if price <= 10000:
         pdf.multi_cell(180, 7, txt=str(num) + ". Zbierz 3 konkurencyjne oferty przedmiotu zakupu. W przypadku usług"
@@ -121,7 +129,7 @@ if rule_num != 0:
                                               " wykonawcy, notatka z przeprowadzonej rozmowy telefonicznej z wykonawcą"
                                               " lub pisemna oferta złożona przez wykonawcę.")
         num += 1
-    elif price<= 50000:
+    elif price <= tender_threshold_nonresearch_euro:
         pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij zaproszenie do składania ofert (zaproszenie.odt) oraz opis"
                                               " przedmiotu zamówienia (opis-przedmiotu-zamowienia.odt). Załącz"
                                               " wzór propozycji cenowej (wzor-propozycji-cenowej.odt) i taki"
@@ -139,68 +147,117 @@ if rule_num != 0:
         num += 1
         pdf.multi_cell(180, 7, txt=str(num) + ". Zbierz oferty przesłane przez firmy.")
         num += 1
+    elif research == 'tak' and price > tender_threshold_nonresearch_euro:
+        pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij zaproszenie do składania ofert (zaproszenie.odt) oraz opis"
+                                              " przedmiotu zamówienia (opis-przedmiotu-zamowienia.odt). Załącz"
+                                              " wzór propozycji cenowej (wzor-propozycji-cenowej.odt) i taki"
+                                              " komplet dokumentów przekaż Pełnomocnikowi Dziekana ds. zamówień"
+                                              " publicznych oraz Dziekanowi (pozostaw w p. 627).")
+        num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Dziekan podpisze zaproszenie. Odbierz dokumenty.")
+        num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Ogłoszenie o udzielanym zamówieniu zamieść w Biuletynie Informacji"
+                                              " Publicznej PW w zakładce 'bez stosowania ustawy'. Ogłoszenie powinno"
+                                              " zawierać: nazwę i adres zamawiającego, adres do korespondencji,"
+                                              " osobę do kontaktu, opis przedmiotu zamówienia, termin wykonania"
+                                              " zamówienia, opis sposobu złożenia ofert, opis sposobu obliczania ceny,"
+                                              " miejsce i termin składania ofert, kryteria oceny ofert i ich znaczenie,"
+                                              " warunki jakie muszą spełniać podmioty zainteresowane wykonaniem"
+                                              " zamówienia.")
+        num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Zeskanuj podpisane zaproszenie. Skan zaproszenia, skan opisu"
+                                              " przedmiotu zamówienia oraz elektroniczną, edytowalną wersję wzoru"
+                                              " propozycji cenowej wyślij mailem do co najmniej 3 firm.")
+        num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Zbierz oferty przesłane przez firmy.")
+        num += 1
 
 
 # Sekcja 3 - raport wykonanych czynności
-if rule_num != 0 :
+if rule_num == 1:
     if price <= 10000:
         pdf.multi_cell(180, 7, txt="NOTATKA SŁUŻBOWA", align="C")
         pdf.multi_cell(180, 7, txt=str(num) + ". Na podstawie wykonanego przeglądu ofert uzupełnij notatkę służbową"
                                               " (notatka-sluzbowa.odt). Wydrukuj i podpisz.")
         num += 1
-    else:
+    elif price <= tender_threshold_nonresearch_euro:
         pdf.multi_cell(180, 7, txt="RAPORT WYKONANYCH CZYNNOŚCI", align="C")
         pdf.multi_cell(180, 7, txt=str(num) + ". Na podstawie wykonanego przeglądu ofert uzupełnij raport wykonanych"
                                               " czynności (protokol-wykonanych-czynnosci.odt). Nie zmieniaj kursu euro"
                                               " w raporcie. Wydrukuj i podpisz. W punkcie 9 raportu należy zebrać"
-                                              " podpisy 3 pracowników ZiF. ")
+                                              " podpisy 3 pracowników Instytutu. ")
+        num += 1
+    elif research == 'tak' and price > tender_threshold_nonresearch_euro:
+        pdf.multi_cell(180, 7, txt=str(num) + ". Na podstawie wykonanego przeglądu ofert uzupełnij raport wykonanych"
+                                          " czynności (protokol-wykonanych-czynnosci.odt). Nie zmieniaj kursu euro"
+                                          " w raporcie. Wydrukuj i podpisz. W punkcie 9 raportu należy zebrać"
+                                          " podpisy 3 pracowników Instytutu. ")
+        num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Przekaż raport do zatwierdzenia przez Dziekana. ")
         num += 1
 
+
 # Sekcja 4 - podpisanie zamówienia, umowy
-if rule_num != 0:
+if rule_num == 1:
     if price <= 10000:
         pdf.multi_cell(180, 7, txt="PODPISANIE ZAMÓWIENIA", align="C")
-        pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij zamówienie (zamowienie.odt). Złóż dwie kopie zamówienia,"
-                                              " jedną kopię notatki służbowej i wydrukowane oferty do Dyrektora. Odbierz"
-                                              " podpisane kopie zamówienia. Wyślij oryginał lub skan firmie"
-                                              " realizującej dostawę/usługę. Firma powinna podpisać dokumenty i"
-                                              " odesłać jedną kopię przed realizacją dostawy/usługi.")
+        pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij zamówienie (zamowienie.odt).")
         num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Złóż dwie kopie zamówienia, jedną kopię notatki służbowej i wydrukowane oferty do Dyrektora.")
+        num += 1
+        pdf.multi_cell(180, 7, txt=str(num) + ". Odbierz podpisane kopie zamówienia.")
+        num += 1
+        if research == 'tak':
+            pdf.multi_cell(180, 7, txt=str(num) + ". Oryginał lub skan zamówienia wyślij firmie"
+                                                  " realizującej dostawę/usługę razem z dokumentem RODO (RODO-badawczy.odt). Firma powinna podpisać dokumenty i"
+                                                  " odesłać jedną kopię zamówienia i dokument RODO przed realizacją dostawy/usługi.")
+            num += 1
+        else:
+            pdf.multi_cell(180, 7, txt=str(num) + ". Oryginał lub skan zamówienia wyślij firmie"
+                                                  " realizującej dostawę/usługę razem z dokumentem RODO (RODO.odt). Firma powinna podpisać dokumenty i"
+                                                  " odesłać jedną kopię zamówienia i dokument RODO przed realizacją dostawy/usługi.")
+            num += 1
     else:
         pdf.multi_cell(180, 7, txt="PODPISANIE UMOWY", align="C")
         if research == 'tak':
-            pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij umowę (umowa.odt). Złóż dwie kopie umowy, jedną kopię"
-                                                  " raportu wykonanych czynności i wydrukowane oferty do Dyrektora. Odbierz podpisane kopie umowy."
-                                                  " Wyślij obie papierowe kopie firmie realizującej dostawę/usługę. Firma powinna"
-                                                  " podpisać dokumenty i odesłać jedną kopię przed realizacją"
+            pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij umowę (umowa-badawczy.odt).")
+            num += 1
+            if price <= tender_threshold_nonresearch_euro:
+                pdf.multi_cell(180, 7, txt=str(num) + ". Złóż dwie kopie umowy, jedną kopię raportu wykonanych czynności i wydrukowane oferty do Dyrektora.")
+                num += 1
+            else:
+                pdf.multi_cell(180, 7, txt=str(num) + ". Złóż dwie kopie umowy, jedną kopię raportu wykonanych czynności i wydrukowane oferty do Dziekana.")
+                num += 1
+            pdf.multi_cell(180, 7, txt=str(num) + ". Odbierz podpisane kopie umowy.")
+            num += 1
+            pdf.multi_cell(180, 7, txt=str(num) + ". Wyślij obie papierowe kopie firmie realizującej dostawę/usługę razem z dokumentem RODO (RODO-badawczy.odt). Firma powinna"
+                                                  " podpisać dokumenty i odesłać jedną kopię umowy i dokument RODO przed realizacją"
                                                   " dostawy/usługi.")
         else:
-            pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij umowę (umowa-badawczy.odt). Złóż dwie kopie umowy, jedną kopię"
-                                                  " raportu wykonanych czynności i wydrukowane oferty do Dyrektora. Odbierz podpisane kopie umowy."
-                                                  " Wyślij obie papierowe kopie firmie realizującej dostawę/usługę. Firma powinna"
-                                                  " podpisać dokumenty i odesłać jedną kopię przed realizacją"
+            pdf.multi_cell(180, 7, txt=str(num) + ". Wypełnij umowę (umowa.odt).")
+            num += 1
+            pdf.multi_cell(180, 7, txt=str(num) + ". Złóż dwie kopie umowy, jedną kopię raportu wykonanych czynności i wydrukowane oferty do Dyrektora.")
+            num += 1
+            pdf.multi_cell(180, 7, txt=str(num) + ". Odbierz podpisane kopie umowy.")
+            num += 1
+            pdf.multi_cell(180, 7, txt=str(num) + ". Wyślij obie papierowe kopie firmie realizującej dostawę/usługę razem z dokumentem RODO (RODO.odt). Firma powinna"
+                                                  " podpisać dokumenty i odesłać jedną kopię umowy i dokument RODO przed realizacją"
                                                   " dostawy/usługi.")
         num += 1
-
-# TODO: RODO jako dodatek do umowy? (zamówienia też?)
 
 # Sekcja 5 - zakup - faktura przelew lub zwykła
 pdf.multi_cell(180, 7, txt="ZAKUP", align="C")
 if rule_num == 0:
-    pdf.multi_cell(180, 7, txt=str(
-        num) + ". Znajdź interesujący Cię przedmiot i dokonaj zakupu. Możesz zapłacić"
+    pdf.multi_cell(180, 7, txt=str(num) + ". Znajdź interesujący Cię przedmiot i dokonaj zakupu. Możesz zapłacić"
                " własnymi pieniędzmi (po realizacji zakupu dostaniesz zwrot) lub poprosić firmę o fakturę przelewową"
-               " płatną w terminie 14 dni od dnia dostarczenia towaru (faktura zostanie opłacona przez księgowość)."
-               " W obu przypadkach weź fakturę na Instytut!")
+               " płatną w terminie 14 dni od dnia dostarczenia towaru (faktura zostanie opłacona przez księgowość).")
     num += 1
-elif price <= 10000:
-    pdf.multi_cell(180, 7, txt=str(
-        num) + ". Po podpisaniu zamówienia przez firmę, firma może zrealizować dostawę/usługę. Możesz zapłacić"
+elif rule_num == 1 and price <= 10000:
+    pdf.multi_cell(180, 7, txt=str(num) + ". Po podpisaniu zamówienia przez firmę, firma może zrealizować dostawę/usługę. Możesz zapłacić"
                " własnymi pieniędzmi (po realizacji zakupu dostaniesz zwrot) lub poprosić firmę o fakturę przelewową"
-               " płatną w terminie 14 dni od dnia dostarczenia towaru (faktura zostanie opłacona przez księgowość)."
-               " W obu przypadkach upewnij się, że faktura wystawiona będzie na Instytut!")
+               " płatną w terminie 14 dni od dnia dostarczenia towaru (faktura zostanie opłacona przez księgowość).")
     num += 1
-else:
+elif rule_num == 1 and price > 10000:
     pdf.multi_cell(180, 7, txt=str(
         num) + ". Po podpisaniu umowy przez firmę, firma może zrealizować dostawę/usługę. Firma powinna wystawić fakturę przelewową"
                " płatną w terminie 14 dni od dnia dostarczenia towaru (faktura zostanie opłacona przez księgowość)."
@@ -241,11 +298,14 @@ pdf.multi_cell(180, 7, txt=str(
 num += 1
 
 # Sekcja 7 - złożenie faktury do działu administracyjnego
-pdf.multi_cell(180, 7, txt=str(
-        num) + ". Przekaż wszystkie dokumenty do działu księgowego (p. 627).")
+pdf.multi_cell(180, 7, txt=str(num) + ". Przekaż wszystkie dokumenty do działu księgowego (p. 627).")
 num += 1
 
-
+if research == 'tak' and price > tender_threshold_nonresearch_euro:
+    pdf.multi_cell(180, 7, txt=str(num) + ". Umieść na stronie Biuletynu Informacji Publicznej PW ogłoszenie o"
+                                          " udzieleniu zamówienia podając nazwę firmy, z którą zawarto umowę lub"
+                                          " informację o nieudzieleniu zamówienia.")
+    num += 1
 
 print('Plan realizacji zakupu wygenerowany do pliku PDF.')
 
